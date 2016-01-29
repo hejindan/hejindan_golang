a5b05c843b24b45d2dadef262cffe8ef03ae2d9b
何金丹 <hejindan@zhubajie.com>
2016-01-29 14:16:05 +0800

    no

---
+++ calcproj/src/calc/calc.go
@@ -0,0 +1,51 @@
+package main
+import "os"
+import "fmt"
+import "simplemath"
+import "strconv"
+import "github.com/coreos/etcd/etcdmain"
+var Usage = func() {
+	fmt.Println("USAGE:calc command [arguments] ....")
+	fmt.Println("\nThe commands are:\n\tadd\tAddition of two values.\n\tsqrt\tAquareroot of a non-negative value.")
+}
+func main(){
+	args := os.Args
+	if args == nil || len(args) <2{
+	Usage()
+	return
+	}
+//	fmt.Println(args[0],args[1],args[2],args[3],args[4])
+	switch args[1] {
+		case "add" :
+			if len(args) != 4 {
+				fmt.Println("USAGE:calc add <integer1><integer2>")
+				return
+			}
+			v1,err1 := strconv.Atoi(args[2])
+			v2,err2 := strconv.Atoi(args[3])
+			if err1 != nil || err2 != nil {
+				fmt.Println("USAGE:calc add <integer1><integer2>")
+				return
+			}
+			ret := simplemath.Add(v1,v2)
+			fmt.Println("Result: ", ret)
+		case "sqrt":
+			if len(args) != 3 {
+				fmt.Println("USAGE:calc sqrt <integer>")
+				return
+			}
+			v,err := strconv.Atoi(args[2])
+			if err != nil {
+				fmt.Println("USAGE:calc sqrt <integer>")
+				return
+			}
+			ret := simplemath.Sqrt(v)
+			fmt.Println("Result ", ret)
+		default:
+			fmt.Println("not define\n",args[0])
+	}
+}
+
+
+
+
---
+++ test/slice.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+	myarray := [10]int{1,2,3,4,5,6,7,8,9,10}
+	var myslice []int = myarray[:5]
+	fmt.Println("elements of myarray: ")
+	for _,v := range myarray{
+		fmt.Print(v," ")
+	}
+	fmt.Println("\nelements of myslice: ")
+	for _,v := range myslice{
+		fmt.Print(v," ")
+	}
+	fmt.Println()
+
+}
