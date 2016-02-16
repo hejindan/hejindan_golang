b2cbfb3a9057a8cc999980165813b809b8f6f726
何金丹 <hejindan@zhubajie.com>
2016-02-16 16:33:32 +0800

    no

--- test/slice.go
+++ test/slice.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 func main() {
 	myarray := [10]int{1,2,3,4,5,6,7,8,9,10}
-	var myslice []int = myarray[:5]
+	var myslice []int = myarray[5:]
 	fmt.Println("elements of myarray: ")
 	for _,v := range myarray{
 		fmt.Print(v," ")
