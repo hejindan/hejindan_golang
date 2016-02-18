b9582e701133d6ef504ee1e78c979ccf8b9e1f01
何金丹 <hejindan@zhubajie.com>
2016-02-18 09:55:32 +0800

    nt o

--- sorter/src/algorithm/qsort/qsort.go
+++ sorter/src/algorithm/qsort/qsort.go
@@ -4,19 +4,29 @@ import "fmt"
 
 func quicksort(values []int, left, right int) {
 	temp := left
-	//if left == right {
-	//	return
-	//}
+	if left == right {
+		return
+	}
 	for i := left; i <= right; i++ {
 		if values[temp] > values[i] {
-			values[temp], values[i] = values[i], values[temp]
-			temp = i
+			//	values[temp], values[i] = values[i], values[temp]
+			//	temp = i
+			k := values[i]
+			for j := i; j > temp; j-- {
+				values[j] = values[j-1]
+			}
+			values[temp] = k
+			temp = temp + 1
 		}
 		fmt.Printf("i:%d\n", i)
 		fmt.Printf("temp:%d\n", temp)
 	}
-	//quicksort(values, 0, temp-1)
-	//quicksort(values, temp+1, right)
+	if temp != 0 {
+		quicksort(values, 0, temp-1)
+	}
+	if temp != right {
+		quicksort(values, temp+1, right)
+	}
 }
 
 func QuickSort(values []int) {
