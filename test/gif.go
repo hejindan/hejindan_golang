fdbd5e015ab6bc3b25ee2dbd510bb1fd053a59c5
何金丹 <hejindan@zhubajie.com>
2016-02-24 10:07:25 +0800

    add some

--- test/gif.go
+++ test/gif.go
@@ -39,7 +39,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), anim)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 			//, bla, kIndex)
 		}
 		phase += 0.1
